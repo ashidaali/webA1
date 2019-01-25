fd11c8eeea39f92dbb9f30515567220c3d1043ab
ashida.ali <ashida.ali@my.uwi.edu>
2019-01-25 10:40:30 -0400

    Fixed sidenav Styled Nav button

--- js/code.js
+++ js/code.js
@@ -1,8 +1,7 @@
-function openNav()
-{
-    document.getElementById("sidenav").style.width=25;
-}
-function closeNav()
-{
-    document.getElementById("sidenav").style.width=0;
-}
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
